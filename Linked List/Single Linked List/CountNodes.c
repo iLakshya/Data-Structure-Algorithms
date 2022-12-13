490362b618c862b42f08837ec0579397d45ca44c
Lakshya Goel <lgoel340@gmail.com>
2022-12-14 01:30:05 +0530

    Single Linked List

--- Linked List/Single Linked List/CountNodes.c
+++ Linked List/Single Linked List/CountNodes.c
@@ -27,28 +27,35 @@ void createList(int n)
     head = (struct node *)malloc(sizeof(struct node));
     if(head == NULL)
     {
-        printf("\nUnable to allocate memory!");
+        printf("Unable to allocate memory!");
         exit(0);
     }
-    printf("Enter the data of node 1: ");
-    scanf("%d", &data);
-    head -> data = data;
-    head -> next = NULL;
-    temp = head;
-    for(i=2;i<=n;i++)
+    else
     {
-        newNode = (struct node *)malloc(sizeof(struct node));
-        if(newNode == NULL)
-        {
-            printf("\nUnable to allocate memory!");
-            break;
-        }
-        printf("Enter the data of node %d: ", i);
+        printf("Enter the data of node 1: ");
         scanf("%d", &data);
-        newNode -> data = data;
-        newNode -> next = NULL;
-        temp -> next = newNode;
-        temp = temp -> next;
+        head -> data = data;
+        head -> next = NULL;
+        temp = head;
+        for(i=2;i<=n;i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+            if(newNode == NULL)
+            {
+                printf("\nUnable to allocate memory!");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+                newNode -> data = data;
+                newNode -> next = NULL;
+                temp -> next = newNode;
+                temp = temp -> next;
+            }
+        }
+        printf("Linked List Created Successfully!\n");
     }
 }
 int countNodes()
